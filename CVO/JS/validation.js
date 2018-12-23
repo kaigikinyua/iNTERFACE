dd14cb27342f43d5747e9d8c8c3ee47fcc14eecf
kaigikinyua <kaigikinyua@gmail.com>
2018-12-23 03:04:38 +0300

    Validation for javascript

---
+++ CVO/JS/validation.js
@@ -0,0 +1,9 @@
+function empty(){
+    var username=document.forms.only.uname.value;
+    var email=document.forms.only.email.value;
+    var password=document.forms.only.uname.value;
+    var cpassword=document.forms.only.uname.value;
+    if(username){
+
+    }
+}
