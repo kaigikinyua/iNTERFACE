52cca6949ef213caea54c3c905a982fcf900d297
kaigikinyua <kaigikinyua@gmail.com>
2018-12-24 02:44:49 +0300

    added a new userpage and did some small interface design

--- CVO/JS/validation.js
+++ CVO/JS/validation.js
@@ -1,9 +1,27 @@
+console.log("Hello");
 function empty(){
-    var username=document.forms.only.uname.value;
-    var email=document.forms.only.email.value;
-    var password=document.forms.only.uname.value;
-    var cpassword=document.forms.only.uname.value;
-    if(username){
-
+    var username=document.forms["only"]["uname"].value;
+    var email=document.only.email.value;
+    var password=document.only.pass.value;
+    var cpassword=document.only.passc.value;
+    if(username==null || username==""){
+        alert("Please fill in your username")
+        return false;
+    }
+    else if(email==null || email ==""){
+        alert("Please fill in your Email Address");
+        return false;
+    }
+    else if(password==null || password==""){
+        alert("Please fill in the password field");
+        return false;
+    }
+    else if(cpassword==null || cpassword==""){
+        alert("Please fill in the confirm password field");
+        return false;
+    }
+    else{
+        console.log(username);
+        return false;
     }
 }
