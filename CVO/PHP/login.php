a73d651f4a65f4086677618580516e44536a2f31
kaigikinyua <kaigikinyua@gmail.com>
2019-01-06 22:36:42 +0300

    login + signup done

--- CVO/PHP/login.php
+++ CVO/PHP/login.php
@@ -2,7 +2,18 @@
     if(isset($_POST['login'])){
         $name=$_POST['uname'];$pass=$_POST['pass'];
         if(!empty($name) && !empty($pass)){
-            header("location:../userpage.html");
+            $con=mysqli_connect("localhost","root","root","CVO");
+            if($con){
+                $login="SELECT * FROM cvo_users where email='$name' and password='$pass'";
+                $r=mysqli_query($con,$login);
+                ##<!!VALIDATE EMAIL >
+                if(mysqli_num_rows($r)==1){
+                    header("location:../userpage.html");
+                }
+                else{
+                    echo("wrong credentials");
+                }
+            }
         }else{
             header("location:../login.html");
             echo "<script>alert('Please fill in all the fields')</script>";
