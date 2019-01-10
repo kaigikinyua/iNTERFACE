eb997c6929c8a03e36913fa1ff75ebfa639bf6d8
kaigikinyua <kaigikinyua@gmail.com>
2019-01-10 23:41:28 +0300

    added file size limit

--- CVO/PHP/upload.php
+++ CVO/PHP/upload.php
@@ -2,7 +2,12 @@
     if(isset($_POST['submit_profile'])){
         #check file size,uniqueid,insert path to the database
         $file=$_FILES['pp']['tmp_name'];
-        print_r ($file);
-        move_uploaded_file($file,'../USER/'.$_FILES['pp']['name']);
+        print_r ($_FILES['pp']);
+        if($_FILES['pp']['size']>5000000){
+            echo "File is too big";
+        }else{
+            move_uploaded_file($file,'../USER/'.$_FILES['pp']['name']);
+
+        }
     }
 ?>
